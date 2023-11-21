0a5ba7bd2ecad8c5a6a66531ee9046d6fe80b4e0
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-11-21 15:58:33 +0100

    Added posts database structure

--- app/config.php
+++ app/config.php
@@ -14,15 +14,28 @@ if (!$db_connect) {
     $db_connect = mysqli_connect($server, $login, $password, $database);
 
     $users_create_tab = mysqli_query($db_connect, "CREATE TABLE IF NOT EXISTS `users` (
-        `User_Id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-        `UserFirstName` varchar(50) NOT NULL,
-        `UserLastName` varchar(80) NOT NULL,
+        `User_Id` INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `UserFirstName` VARCHAR(50) NOT NULL,
+        `UserLastName` VARCHAR(80) NOT NULL,
         `UserGender` ENUM('M','F') NOT NULL DEFAULT 'M',
-        `UserEmail` varchar(255) NOT NULL,
-        `UserPassword` varchar(255) NOT NULL,
-        `UserImg` varchar(400) DEFAULT NULL,
-        `UserStatus` tinyint(1) DEFAULT NULL,
+        `UserEmail` VARCHAR(255) NOT NULL,
+        `UserPassword` VARCHAR(255) NOT NULL,
+        `UserImg` VARCHAR(400) DEFAULT NULL,
+        `UserStatus` TINYINT(1) DEFAULT NULL,
         PRIMARY KEY (`User_Id`),
         UNIQUE KEY `UserEmail` (`UserEmail`)
       )");
+
+    $posts_create_tab = mysqli_query($db_connect, "CREATE TABLE IF NOT EXISTS `posts` (
+        `Id` INT(50) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `Post_Id` INT(50) NOT NULL,
+        `User_Id` INT(20) NOT NULL,
+        `Post` TEXT NOT NULL,
+        `UserImg` VARCHAR(400) NULL,
+        `Comments` INT(11) NULL,
+        `Likes` INT(11) NOT NULL DEFAULT 0,
+        `Date` TIMESTAMP NOT NULL,
+        PRIMARY KEY (`Id`),
+        UNIQUE KEY (`Post_Id`)
+    )");
 }
